123e240950248e007e1665633f2c2e81e5035aef
widley95 <widley95@gmail.com>
2017-04-06 06:21:05 -0400

    Fixed merged conflicts

--- src/userInterface/BackgammonUI.java
+++ src/userInterface/BackgammonUI.java
@@ -28,9 +28,15 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-
-import com.sun.javafx.font.Disposer;
-
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.plaf.ButtonUI;
+import javax.swing.plaf.basic.BasicButtonUI;
+import javax.swing.plaf.metal.MetalButtonUI;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 
 public class BackgammonUI
@@ -40,6 +46,11 @@ public class BackgammonUI
 	public 	JComponent mainContainer; 
 
 	
+	
+	private  JFrame gameFrame; 
+	public int flag = 0;
+	
+
 	public BackgammonUI() throws IOException
 	{
 		initComponents();
@@ -66,6 +77,12 @@ public class BackgammonUI
 		// font to be used by the buttons on the home page
 		buttonsFont = new Font("Arial", Font.BOLD, 30); 
 		
+
+		
+		
+		// font to be used by the buttons on the home page
+		buttonsFont = new Font("Arial", Font.BOLD, 30); 
+
 		// set up different jpanel for the buttons 
 		buttonsPanel = new JPanel();
 		buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.Y_AXIS));
@@ -112,6 +129,7 @@ public class BackgammonUI
 	
 	
 		imageLabel.setBorder(BorderFactory.createRaisedBevelBorder());
+	
 		imageLabel.setMaximumSize(new Dimension(100, 100));
 						
 		// add everything to the main container 
@@ -119,10 +137,23 @@ public class BackgammonUI
 		mainContainer.add(imageLabel, BorderLayout.CENTER);
 		mainContainer.add(Box.createRigidArea(new Dimension(50,50)), BorderLayout.PAGE_START);
 		mainContainer.add(Box.createRigidArea(new Dimension(50,50)), BorderLayout.PAGE_END);
+
 	
 		boardFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		boardFrame.setResizable(false);
 		boardFrame.setVisible(true);
+
+		
+		Border border = mainContainer.getBorder();
+		Border margin = new EmptyBorder(10,30,10,10);
+		mainContainer.setBorder(new CompoundBorder(border, margin));
+		
+		
+	
+		boardFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		boardFrame.setResizable(false);
+		boardFrame.setVisible(true); 
+
 	}
 	
 	
@@ -151,6 +182,7 @@ public class BackgammonUI
 			Font difficutlyFonts = new Font("Tahoma", 1, 15);
 			Font playToFonts = new Font("Tahoma", 1, 15);
 			JDialog playerDialogBox = new JDialog(boardFrame, buttonClicked, true);
+
 			playerDialogBox.setSize(new Dimension(650,400));
 			playerDialogBox.setLocationRelativeTo(boardFrame);
 			playerDialogBox.setAlwaysOnTop(true);
@@ -162,6 +194,7 @@ public class BackgammonUI
 			
 			JTextField playerOneNameField = new JTextField();
 			playerOneNameField.setMaximumSize(new Dimension(350, 40));
+			
 			playerOneNameField.setText("Player 1");
 			playerOneNameField.setBorder(BorderFactory.createRaisedSoftBevelBorder());
 			JLabel nameOneLabel = new JLabel("Name");
@@ -186,6 +219,7 @@ public class BackgammonUI
 			playToFive.setFont(playToFonts);
 			JButton playToSeven = new JButton("7"); 
 			playToSeven.setFont(playToFonts);
+			/*
 			JButton whiteColorButton = new JButton();
 			whiteColorButton.setBackground(Color.WHITE);
 			whiteColorButton.setOpaque(true);
@@ -195,9 +229,26 @@ public class BackgammonUI
 			blackColorButton.setOpaque(true);
 			blackColorButton.setBorderPainted(false);
 			blackColorButton.setForeground(Color.blue);
+*/
+			playerOneNameField.setText("Player 1");
+			playerOneNameField.setBorder(BorderFactory.createRaisedSoftBevelBorder());
+			 
+	
+			JButton whiteColorButton = new JButton();
+			whiteColorButton.setBackground(Color.WHITE);
+			whiteColorButton.setUI( new BasicButtonUI());
+			whiteColorButton.setOpaque(true);
+//			whiteColorButton.setBorderPainted(true);
+			
+			JButton blackColorButton = new JButton(); 
+			blackColorButton.setUI( new BasicButtonUI( ));
+			blackColorButton.setBackground(Color.BLACK);
+			blackColorButton.setOpaque(true);
+//			blackColorButton.setBorderPainted(true);
+			
 			JButton startButton = new JButton("Start");
 			startButton.setToolTipText("Start Game!");
-			startButton.addActionListener(new GameListener());
+			startButton.addActionListener(new GameListener(playerDialogBox));
 			startButton.setFont(buttonFonts);
 			JButton backButton = new JButton("Back"); 
 			backButton.setFont(buttonFonts);
@@ -438,11 +489,207 @@ public class BackgammonUI
 	/*ANDRES, start doing your shit here*/
 	private class GameListener implements ActionListener
 	{
+		private JDialog dialogBox;
+		GameListener()
+		{
+			
+		}
+		GameListener(JDialog dialogBox)
+		{
+			this.dialogBox = dialogBox;
+		}
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			dialogBox.dispose();
+			mainContainer.removeAll();
+			mainContainer.revalidate();
+			mainContainer.repaint();
+			
+			JPanel buttonsPanel1; 
+			JButton resetButton; 
+			JButton quitButton; 
+			JButton mainMenuButton;
+			Font buttonsFont; 
+			JLabel boardLabel = new JLabel(); 
+			
+			
+		
+			
+			boardLabel = new JLabel(); 
+			// resize the image icon so it can fit in the jlabel 
+			boardLabel.setIcon(new ImageIcon(new javax.swing.ImageIcon(getClass().getResource("board.jpg"))
+					.getImage().getScaledInstance(790, 750, Image.SCALE_SMOOTH)));
+			
+		
+			boardLabel.setBorder(BorderFactory.createRaisedBevelBorder()); //remove this border
+			boardLabel.setMaximumSize(new Dimension(100, 100));
+			
+			buttonsFont = new Font("Arial", Font.BOLD, 30); 
+			
+			// set up different jpanel for the buttons 
+			buttonsPanel1 = new JPanel();
+			buttonsPanel1.setLayout(new BoxLayout(buttonsPanel1, BoxLayout.Y_AXIS));
+			buttonsPanel1.setPreferredSize(new Dimension(350, 10));
+			buttonsPanel1.add(Box.createRigidArea(new Dimension(0,300)));
+			buttonsPanel1.setBackground(Color.DARK_GRAY);
+		
+			resetButton = new JButton("Reset");
+			resetButton.setToolTipText("Start Fresh!");
+			resetButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+			resetButton.setMaximumSize(new Dimension(250,70));
+			resetButton.setActionCommand("Reset");
+			resetButton.setFont(buttonsFont);
+			resetButton.addActionListener(new GameListener());
+			
+			
+			mainMenuButton = new JButton("Main Menu");
+			mainMenuButton.setToolTipText("Back to Main Menu!");
+			mainMenuButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+			mainMenuButton.setMaximumSize(new Dimension(250,70));
+			mainMenuButton.setActionCommand("Main Menu");
+			mainMenuButton.setFont(buttonsFont);
+			
+			 
+			
+			buttonsPanel1.add(resetButton);
+			buttonsPanel1.add(Box.createRigidArea(new Dimension(0,15)));
+			
+			
+			
+			// quit player button 
+			quitButton = new JButton("Quit");
+			quitButton.setToolTipText("Go Back to Main Screen");
+			quitButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+			quitButton.setMaximumSize(new Dimension(250, 70));
+			quitButton.setActionCommand("Quit");
+			quitButton.setFont(buttonsFont);
+			
+			
+			
+			buttonsPanel1.add(mainMenuButton);
+			buttonsPanel1.add(Box.createRigidArea(new Dimension(0,15)));
+			
+			
+			
+			buttonsPanel1.add(quitButton);
+			buttonsPanel1.add(Box.createRigidArea(new Dimension(0,15)));
+			
+			
+			
+			
+							
+			// add everything to the main container 
+			
+			mainContainer.add(buttonsPanel1, BorderLayout.LINE_END);
+			mainContainer.add(boardLabel, BorderLayout.CENTER);
+			mainContainer.add(Box.createRigidArea(new Dimension(50,50)), BorderLayout.PAGE_START);
+			mainContainer.add(Box.createRigidArea(new Dimension(50,50)), BorderLayout.PAGE_END);
+			
+		//	Border border = mainContainer.getBorder();
+			//Border margin = new EmptyBorder(10,30,10,10);
+		//	mainContainer.setBorder(new CompoundBorder(border, margin));
+			
+			quitButton.addActionListener(new QuitListener());
+			resetButton.addActionListener(new GameListener()); 
+			mainMenuButton.addActionListener(new mainMenuListener());
+			
+			
+			
+			boardFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			boardFrame.setResizable(false);
+			boardFrame.setVisible(true);
+			
+
+		}
+	}
+	
+
+
+	private class mainMenuListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e)
 		{
 			
+			JPanel buttonsPanel; 
+			JButton singlePlayerButton; 
+			JButton multiPlayerButton; 
+			JButton quitButton; 
+			Font buttonsFont; 
+			JLabel imageLabel; 
+			
+			mainContainer.removeAll();
+			mainContainer.revalidate();
+			mainContainer.repaint();
+			
+			
+			// font to be used by the buttons on the home page
+			buttonsFont = new Font("Arial", Font.BOLD, 30); 
+			
+			
+			// set up different jpanel for the buttons 
+			buttonsPanel = new JPanel();
+			buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.Y_AXIS));
+			buttonsPanel.setPreferredSize(new Dimension(350, 10));
+			buttonsPanel.add(Box.createRigidArea(new Dimension(0,300)));
+			buttonsPanel.setBackground(Color.DARK_GRAY);
+			
+			// single player button 
+			singlePlayerButton = new JButton("Single");
+			singlePlayerButton.setToolTipText("Click to play against the computer");
+			singlePlayerButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+			singlePlayerButton.setMaximumSize(new Dimension(250,70));
+			singlePlayerButton.setActionCommand("Single");
+			singlePlayerButton.setFont(buttonsFont);
+			singlePlayerButton.addActionListener(new PlayerListener());
+			buttonsPanel.add(singlePlayerButton);
+			buttonsPanel.add(Box.createRigidArea(new Dimension(0,15)));
+			
+			// multi player button 
+			multiPlayerButton = new JButton("Multiplayer");
+			multiPlayerButton.setToolTipText("Click to play against a friend");
+			multiPlayerButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+			multiPlayerButton.setMaximumSize(new Dimension(250, 70));
+			multiPlayerButton.setActionCommand("Multiplayer");
+			multiPlayerButton.setFont(buttonsFont);
+			multiPlayerButton.addActionListener(new PlayerListener());
+			buttonsPanel.add(multiPlayerButton);
+			buttonsPanel.add(Box.createRigidArea(new Dimension(0,15)));
+			
+			// quit button 
+			quitButton = new JButton("Quit"); 
+			quitButton.setToolTipText("Click to Quit game");
+			quitButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+			quitButton.setMaximumSize(new Dimension(250, 70));
+			quitButton.setFont(buttonsFont);
+			quitButton.addActionListener(new QuitListener());
+			buttonsPanel.add(quitButton);
+			
+			// seperate label for background image
+			imageLabel = new JLabel(); 
+			// resize the image icon so it can fit in the jlabel 
+			imageLabel.setIcon(new ImageIcon(new javax.swing.ImageIcon(getClass().getResource("Backgammonboard.jpg"))
+					.getImage().getScaledInstance(790, 750, Image.SCALE_SMOOTH)));
+			
+		
+			imageLabel.setBorder(BorderFactory.createRaisedBevelBorder()); //remove this border
+			imageLabel.setMaximumSize(new Dimension(100, 100));
+							
+			// add everything to the main container 
+			mainContainer.add(buttonsPanel, BorderLayout.LINE_END);
+			mainContainer.add(imageLabel, BorderLayout.CENTER);
+			mainContainer.add(Box.createRigidArea(new Dimension(50,50)), BorderLayout.PAGE_START);
+			mainContainer.add(Box.createRigidArea(new Dimension(50,50)), BorderLayout.PAGE_END);
+			
+			
+			boardFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			boardFrame.setResizable(false);
+			boardFrame.setVisible(true); 
+			
+			
 		}
-	}
+	
 	
 }
+}
+	
